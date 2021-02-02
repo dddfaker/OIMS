545db26ca31018ffe9e657b176c52922850748f4
Banana-Boat <937499953@qq.com>
2021-02-02 21:58:42 +0800

    '初始化页面布局'

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '骨科影像智能量测系统'
+  }
+</script>
+
+<style>
+  body{
+    margin: 0;
+    height: 100vh;
+    font-family: "微软雅黑";
+  }
+  #app{
+    height: 100%;
+  }
+</style>
